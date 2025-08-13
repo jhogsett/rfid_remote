2a0092c8ffb1d765e2fde482dc074ed3fc9ea98a
Jerry Hogsett <jhogsett@gmail.com>
2025-08-13 03:15:41 -0700

    likes 590

--- ook_radios/ards_receive_display/ards_receive_display.ino
+++ ook_radios/ards_receive_display/ards_receive_display.ino
@@ -15,9 +15,13 @@
 
 #define CLIENT_ADDRESS 1
 #define SERVER_ADDRESS 2
+
+#define DATARATE 590
+#define RETRIES 15
+#define TIMEOUT 1000
  
 // Singleton instance of the radio driver
-RH_ASK driver(850);
+RH_ASK driver(DATARATE);
 // RH_ASK driver(2000, 4, 5, 0); // ESP8266 or ESP32: do not use pin 11 or 2
 // RH_ASK driver(2000, PD14, PD13, 0); STM32F4 Discovery: see tx and rx on Orange and Red LEDS
  
@@ -33,12 +37,15 @@ void setup()
   if (!manager.init())
     Serial.println("init failed");
 
+  manager.setRetries(RETRIES);
+  manager.setTimeout(TIMEOUT);
+
   Wire.begin();
   byte brightness1[3] = { DISPLAY_BRIGHTNESS, DISPLAY_BRIGHTNESS, DISPLAY_BRIGHTNESS };
   disp1.init(brightness1);
 }
  
-uint8_t data[] = "And hello back to you";
+uint8_t data[] = "!!!";
 // Dont put this on the stack:
 uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
  
@@ -62,11 +69,17 @@ void loop()
 
       buf[len] = '\0';
 
+      int resend_count = manager.retransmissions();
+
       char dispbuf[30];
-      sprintf(dispbuf, "%s %d %d", (char*)buf, recvcount, failcount);
+      // sprintf(dispbuf, "%s %d %d", (char*)buf, recvcount, failcount);
+      sprintf(dispbuf, "R%d F%d R%d", recvcount, failcount, resend_count);
       Serial.println(dispbuf);
 
       disp1.scroll_string(dispbuf);
+
+      // Serial.print("Resends: ");
+      // Serial.println(resend_count);
 
       // Send a reply back to the originator client
       if (!manager.sendtoWait(data, sizeof(data), from)){
